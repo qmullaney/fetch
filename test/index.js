efc20eaed3a52f153065719e884c63317b7067c2
Quinn Mullaney <skiingqm@gmail.com>
2022-01-20 22:04:24 -0500

    test

---
+++ test/index.js
@@ -0,0 +1,24 @@
+
+ 
+
+// describe('Points API', function() {
+    
+//     it('return the correct points', function(done) {
+        
+//         let transaction = { "payer": "DANNON", "points": -100 };
+//         let res = addTransaction(t);
+
+//         if (res == [{ "payer": "DANNON", "points": -100 }]) {
+
+//             done();
+
+//         } else {
+
+//             done(new Error(res));
+
+//         }
+  
+//     });
+  
+//   });
+
